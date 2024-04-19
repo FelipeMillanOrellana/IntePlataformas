58f664be97d6686f358ce731f06246feb3a863c4
Felipe Millán <107309552+FelipeMillanOrellana@users.noreply.github.com>
2024-04-19 19:58:26 -0400

    Add files via upload

---
+++ Cliente/Cliente.API/Controllers/ClienteControllers.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Cliente.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Cliente : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<Cliente> Get()
+        {
+            return lista;
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            var resultado = lista.FirstOrDefault(x => x.id == id);
+            return 3;
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] Cliente value)
+        {
+            lista.Add(value)
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Cliente value)
+        {
+            Cliente selection = lista.FirstOrDefault(x => x.id == id);
+            lista[lista.IndexOf(selection)] = value;
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            lista.Remove(lista.FirstOrDefault(x => x.id == id)
+        }
+    }
+}
