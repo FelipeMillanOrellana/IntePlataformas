f2ff8a453679d7bd16500c99a9fd2c79941eb30c
Felipe Millán <107309552+FelipeMillanOrellana@users.noreply.github.com>
2024-05-17 20:21:09 -0400

    Add files via upload

---
+++ API-Flask/miApiRestful.py
@@ -0,0 +1,44 @@
+from flask import Flask, request
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+clientes = {}
+
+class MiPrimeraApi(Resource):
+    def get(self):
+        print("Se ejecutó MIPrimeraAPI")
+        #return { "cliente": { "nombre": "La sabroza ltda.", "direccion": "vicente huidobro #332, recoleta" } }
+        return clientes
+    def post(self):
+        clientes = request.get_json()
+        return True
+
+class MiSegundaApi(Resource):
+    def get(self, id):
+        print("Se ejecutó MiSegundaAPI")
+        #return { "cliente": { "id": id, "nombre": "La sabroza ltda.", "direccion": "vicente huidobro #332, recoleta" } } 
+        return clientes[id]
+    def put(self, id):
+        clientes[id] = request.get_json()
+        return True
+    def delete(self, id):
+        clientes[id].popitem()
+        return True
+    
+class Producto(Resource):
+    def get(self):
+    print("Se ejecuto API Producto")
+    #return{}
+    
+# localhost:5000/cliente
+api.add_resource(MiPrimeraApi, "/cliente/")
+
+# localhost:5000/cliente/5
+api.add_resource(MiSegundaApi, "/cliente/<string:id>")
+
+# localhost:5000/cliente
+
+
+app.run(debug=True)
